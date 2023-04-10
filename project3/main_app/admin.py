8e731e8622016c3cf4b4045253cd37b2f37ac6d1
erictheericx1 <e.lopez.7.el@gmail.com>
2023-04-09 23:33:58 -0700

    readme screenshot fix

--- project3/main_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AppUser, Activity, UserActivity, Wish
-
-# Register your models here.
-admin.site.register(AppUser)
-admin.site.register(Activity)
-admin.site.register(UserActivity)
-admin.site.register(Wish)
